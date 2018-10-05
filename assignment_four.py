bbc01b2549d04d89f06cf75447198f494a31c3e4
Yerim <yerim@iMac6.local>
2018-10-05 14:15:59 -0400

    Today I added the if and elif parts of my program

--- assignment_four.py
+++ assignment_four.py
@@ -5,18 +5,31 @@ def get_problem_type():
     problem_type = input("What type of problem do you want?")
     return problem_type
 
+
 def get_max_number():
     max_number = float(input("What is the max number you want?"))
     return max_number
 
+
 def random_number(max_number):
-    return random.randint (1, max_number)
-    return random.randint (1, max_number)
+    return random.randint(1, max_number)
+
+
+def correct_answer(first_number, second_number, problem)
+    if problem == "*":
+        return first_number * second_number
+    elif problem == "+":
+
+
 def main():
     problem = get_problem_type()
     number = get_max_number()
     first_number = random_number(number)
     second_number = random_number(number)
+    print("Your problem is:" ,first_number, problem, second_number)
+    user_answer = float(input("What is the answer?"))
+    if user_answer = (first_number), problem, (second_number):
+
 
 
 
