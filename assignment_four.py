456cb05a75cb48a079a4149996c0f5b275ec6041
Yerim <yerim@iMac6.local>
2018-10-08 15:13:22 -0400

    Today I finalized all my code and made sure yo check for any errors. I just need to finish the doctstrings.

--- assignment_four.py
+++ assignment_four.py
@@ -1,3 +1,11 @@
+# Yerim Dia
+# October 8, 2018
+# Introduction to Computer Science
+# This program allows the user to practice mathematical problems.
+# The minimum number is 1 and the user inputs their desired max number and the desired problem.
+# The program then takes two numbers between 1 and max and displays the problem.
+
+
 import random
 
 
@@ -15,10 +23,25 @@ def random_number(max_number):
     return random.randint(1, max_number)
 
 
-def correct_answer(first_number, second_number, problem)
+def correct_answer(first_number, second_number, problem):
     if problem == "*":
         return first_number * second_number
-    elif problem == "+":
+    elif problem == "-":
+        return first_number - second_number
+    else:
+        return first_number + second_number
+
+
+def user_answer(first_number, second_number, problem):
+    if problem == "*":
+        operator = "*"
+    elif problem == "-":
+        operator = "-"
+    else:
+        operator = "+"
+    print("Your problem is:", first_number, operator, second_number)
+    answer = int(input("What is the answer?"))
+    return answer
 
 
 def main():
@@ -26,12 +49,12 @@ def main():
     number = get_max_number()
     first_number = random_number(number)
     second_number = random_number(number)
-    print("Your problem is:" ,first_number, problem, second_number)
-    user_answer = float(input("What is the answer?"))
-    if user_answer = (first_number), problem, (second_number):
-
-
-
+    answer = user_answer(first_number, second_number, problem)
+    correct = correct_answer(first_number, second_number, problem)
+    if answer == correct:
+        print("Nice job you found the answer")
+    else:
+        print("Too bad the answer was", correct_answer(first_number, second_number, problem))
 
 
 main()
