7bade6e2dcde55d2a13679ec4ab2006d90133301
Yerim <yerim@iMac6.local>
2018-10-08 14:36:11 -0400

    if, elif, else

---
+++ d4_unit_four_warmups.py
@@ -0,0 +1,7 @@
+age = int(input("How old are you?"))
+if age <=2:
+    print("You can go in for free")
+elif age >2 and age <=12:
+    print("The price for you is $8")
+else:
+    print("Your ticket is $10")
